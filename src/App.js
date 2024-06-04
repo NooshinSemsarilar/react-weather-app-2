91cbf68c98043149b33c9bac432abcbc0ca53b31
Nooshin Semsarilar <148267945+NooshinSemsarilar@users.noreply.github.com>
2024-06-04 20:40:04 +0330

    finalize the structure

--- src/App.js
+++ src/App.js
@@ -1,13 +1,12 @@
-import logo from './logo.svg';
+
 import './App.css';
+import Weather from './Weather';
 
 export default function App() {
   return (
     <div className="App">
       <div className='container'>
-    <h1>
-    Weather App
-    </h1>
+    <Weather />
     <footer>
       This project is created by Nooshin Semsarilar and It is <a href='https://github.com/NooshinSemsarilar/react-weather-app-2' target='-blank'>open-sourced on github</a>
     </footer>
